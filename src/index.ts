60ba6075509844432bc85dc754fbb439808926ac
Sakibul Islam <sakibulislam25800@gmail.com>
2024-08-29 08:58:39 +0100

    Add Airtable OAuth endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,46 @@
+import crypto from "node:crypto";
 import { generateRandomId } from "./generateRandomId";
 import { getHTMLTemplate } from "./getHTMLTemplate";
 
 async function handleRequest(request: Request, env: Env) {
   const requestUrl = new URL(request.url);
 
-  // Generate an authorization URL to login into the provider, and a set of
-  // read and write keys for retrieving the access token later on.
   if (
     request.method === "POST" &&
-    requestUrl.pathname.startsWith("/authorize")
+    requestUrl.pathname.startsWith("/auth/authorize")
   ) {
     const readKey = generateRandomId();
     const writeKey = generateRandomId();
+    const codeVerifier = crypto.randomBytes(96).toString("base64url");
 
-    const authorizeParams = new URLSearchParams();
-    authorizeParams.append("client_id", env.CLIENT_ID);
-    authorizeParams.append("redirect_uri", env.REDIRECT_URI);
-    authorizeParams.append("response_type", "code");
-    authorizeParams.append("access_type", "online");
-    authorizeParams.append("include_granted_scopes", "true");
+    const codeChallenge = crypto
+      .createHash("sha256")
+      .update(codeVerifier)
+      .digest("base64")
+      .replace(/=/g, "")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_");
 
-    // Scope is optional for some providers.
-    if (env.SCOPE) {
-      authorizeParams.append("scope", env.SCOPE);
-    }
+    const authorizeParams = new URLSearchParams({
+      code_challenge: codeChallenge,
+      code_challenge_method: "S256",
+      client_id: env.CLIENT_ID,
+      redirect_uri: env.REDIRECT_URI,
+      response_type: "code",
+      state: writeKey,
+      scope: env.SCOPE || "",
+    });
 
-    // The write key is stored in the `state` param since this will be
-    // persisted through the entire OAuth flow.
-    authorizeParams.append("state", writeKey);
+    const authorizeUrl = `${env.AUTHORIZE_ENDPOINT}?${authorizeParams}`;
 
-    // Generate the login URL for the provider.
-    const authorizeUrl = new URL(env.AUTHORIZE_ENDPOINT);
-    authorizeUrl.search = authorizeParams.toString();
-
-    await env.keyValueStore.put(`readKey:${writeKey}`, readKey, {
+    await env.airtableOAuthStore.put(`readKey:${writeKey}`, readKey, {
+      expirationTtl: 60,
+    });
+    await env.airtableOAuthStore.put(`codeVerifier:${writeKey}`, codeVerifier, {
       expirationTtl: 60,
     });
 
-    const response = JSON.stringify({
-      url: authorizeUrl.toString(),
-      readKey,
-    });
-
-    return new Response(response, {
+    return new Response(JSON.stringify({ url: authorizeUrl, readKey }), {
       headers: {
         "Content-Type": "application/json",
         "Access-Control-Allow-Origin": env.PLUGIN_URI,
@@ -50,76 +48,72 @@ async function handleRequest(request: Request, env: Env) {
     });
   }
 
-  // Once the user has been authorized via login page, the provider will
-  // redirect them back this URL with an access code (not an access token) and
-  // the write key we stored in the state param.
-  if (request.method === "GET" && requestUrl.pathname.startsWith("/redirect")) {
+  if (
+    request.method === "GET" &&
+    requestUrl.pathname.startsWith("/auth/redirect")
+  ) {
     const authorizationCode = requestUrl.searchParams.get("code");
     const writeKey = requestUrl.searchParams.get("state");
 
-    if (!authorizationCode) {
-      return new Response("Missing authorization code URL param", {
-        status: 400,
-      });
+    if (!authorizationCode || !writeKey) {
+      return new Response("Missing required parameters", { status: 400 });
     }
 
-    if (!writeKey) {
-      return new Response("Missing state URL param", {
-        status: 400,
-      });
+    const readKey = await env.airtableOAuthStore.get(`readKey:${writeKey}`);
+    const codeVerifier = await env.airtableOAuthStore.get(
+      `codeVerifier:${writeKey}`
+    );
+
+    if (!readKey || !codeVerifier) {
+      return new Response("Invalid or expired request", { status: 400 });
     }
 
-    // Generate a new URL with the access code and client secret.
-    const tokenParams = new URLSearchParams();
-    tokenParams.append("client_id", env.CLIENT_ID);
-    tokenParams.append("client_secret", env.CLIENT_SECRET);
-    tokenParams.append("redirect_uri", env.REDIRECT_URI);
-    tokenParams.append("grant_type", "authorization_code");
-    tokenParams.append("code", authorizationCode);
+    const tokenParams = new URLSearchParams({
+      client_id: env.CLIENT_ID,
+      code_verifier: codeVerifier,
+      redirect_uri: env.REDIRECT_URI,
+      code: authorizationCode,
+      grant_type: "authorization_code",
+    });
 
-    const tokenUrl = new URL(env.TOKEN_ENDPOINT);
-    tokenUrl.search = tokenParams.toString();
+    const encodedCredentials = btoa(`${env.CLIENT_ID}:${env.CLIENT_SECRET}`);
 
-    // This additional POST request retrieves the access token and expiry
-    // information used for further API requests to the provider.
-    const tokenResponse = await fetch(tokenUrl.toString(), {
+    const tokenResponse = await fetch(env.TOKEN_ENDPOINT, {
       method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${encodedCredentials}`,
+      },
+      body: tokenParams,
     });
 
     if (tokenResponse.status !== 200) {
-      return new Response(tokenResponse.statusText, {
+      return new Response(await tokenResponse.text(), {
         status: tokenResponse.status,
       });
     }
 
-    const readKey = await env.keyValueStore.get(`readKey:${writeKey}`);
-
-    if (!readKey) {
-      return new Response("No read key found in storage", {
-        status: 400,
-      });
-    }
-
-    // Store the tokens temporarily inside a key value store. This will be
-    // retrieved when the plugin polls for them.
-    const tokens = (await tokenResponse.json()) as unknown;
-    env.keyValueStore.put(`tokens:${readKey}`, JSON.stringify(tokens), {
-      expirationTtl: 300,
-    });
+    const tokens = await tokenResponse.json();
+    await env.airtableOAuthStore.put(
+      `tokens:${readKey}`,
+      JSON.stringify(tokens),
+      {
+        expirationTtl: 300,
+      }
+    );
 
     return new Response(
-      getHTMLTemplate(
-        "Authentication successful! You can close this window and return to Framer."
-      ),
+      getHTMLTemplate("Authentication successful! You can close this window."),
       {
-        headers: {
-          "Content-Type": "text/html",
-        },
+        headers: { "Content-Type": "text/html" },
       }
     );
   }
 
-  if (request.method === "POST" && requestUrl.pathname.startsWith("/poll")) {
+  if (
+    request.method === "POST" &&
+    requestUrl.pathname.startsWith("/auth/poll")
+  ) {
     const readKey = requestUrl.searchParams.get("readKey");
 
     if (!readKey) {
@@ -131,7 +125,7 @@ async function handleRequest(request: Request, env: Env) {
       });
     }
 
-    const tokens = await env.keyValueStore.get(`tokens:${readKey}`);
+    const tokens = await env.airtableOAuthStore.get(`tokens:${readKey}`);
 
     if (!tokens) {
       return new Response(null, {
@@ -140,9 +134,7 @@ async function handleRequest(request: Request, env: Env) {
       });
     }
 
-    // Even though the tokens have an expiry, it's important to delete them on
-    // our side to reduce the reliability of storing user's sensitive data.
-    await env.keyValueStore.delete(`tokens:${readKey}`);
+    await env.airtableOAuthStore.delete(`tokens:${readKey}`);
 
     return new Response(tokens, {
       headers: {
@@ -152,7 +144,10 @@ async function handleRequest(request: Request, env: Env) {
     });
   }
 
-  if (request.method === "POST" && requestUrl.pathname.startsWith("/refresh")) {
+  if (
+    request.method === "POST" &&
+    requestUrl.pathname.startsWith("/auth/refresh")
+  ) {
     const refreshToken = requestUrl.searchParams.get("code");
 
     if (!refreshToken) {
@@ -164,27 +159,36 @@ async function handleRequest(request: Request, env: Env) {
       });
     }
 
-    const refreshParams = new URLSearchParams();
-    refreshParams.append("refresh_token", refreshToken);
-    refreshParams.append("client_id", env.CLIENT_ID);
-    refreshParams.append("client_secret", env.CLIENT_SECRET);
-    refreshParams.append("grant_type", "refresh_token");
+    const refreshParams = new URLSearchParams({
+      refresh_token: refreshToken,
+      client_id: env.CLIENT_ID,
+      grant_type: "refresh_token",
+    });
 
-    const refreshUrl = new URL(env.TOKEN_ENDPOINT);
-    refreshUrl.search = refreshParams.toString();
+    const encodedCredentials = btoa(`${env.CLIENT_ID}:${env.CLIENT_SECRET}`);
 
-    const refreshResponse = await fetch(refreshUrl.toString(), {
+    const refreshResponse = await fetch(env.TOKEN_ENDPOINT, {
       method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${encodedCredentials}`,
+      },
+      body: refreshParams,
     });
 
     if (refreshResponse.status !== 200) {
-      return new Response(refreshResponse.statusText, {
+      return new Response(await refreshResponse.text(), {
         status: refreshResponse.status,
       });
     }
 
     const tokens = await refreshResponse.json();
-    return new Response(JSON.stringify(tokens));
+
+    return new Response(JSON.stringify(tokens), {
+      headers: {
+        "Access-Control-Allow-Origin": env.PLUGIN_URI,
+      },
+    });
   }
 
   if (request.method === "GET" && requestUrl.pathname === "/") {
